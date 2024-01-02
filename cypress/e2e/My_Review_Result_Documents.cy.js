cef89b8e62a62327e3d927e9c49269b5ce04a738
Stefanus Hendy Sulistyo <Hendy_S521@mitrais.com>
2024-01-02 09:31:39 +0700

    add automation for download result document

--- cypress/e2e/My_Review_Result_Documents.cy.js
+++ cypress/e2e/My_Review_Result_Documents.cy.js
@@ -13,4 +13,13 @@ describe('Go to My Review Result Documents',() => {
     it('My Review Result Documents table is displayed', () =>{
         ReviewResultDocuments.table_MyReviewResultDocuments().should('exist')
     })
+
+    it('User download review result document', () =>{
+        cy.window().document().then(function (doc) {
+            doc.addEventListener('click', () => {
+              setTimeout(function () { doc.location.reload() }, 5000)
+            })
+            ReviewResultDocuments.link_Download().click()
+          })
+    })
 })
--- cypress/pages/Review Result Documents.js
+++ cypress/pages/Review Result Documents.js
@@ -2,6 +2,10 @@ class ResultDocuments{
     table_MyReviewResultDocuments(){
         return cy.get('#reviewResultDocuments')
     }
+
+    link_Download(){
+        return cy.xpath('//body[1]/div[5]/div[1]/form[1]/div[2]/div[1]/div[2]/div[1]/div[1]/div[4]/div[1]/div[1]/table[1]/tbody[1]/tr[1]/td[4]/a[1]')
+    }
 }
 
 module.exports = new ResultDocuments();
